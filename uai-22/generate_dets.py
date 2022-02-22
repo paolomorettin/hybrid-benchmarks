3b2709eb16a4142a6c6f20cdbdd965cb16a6caa8
paolomorettin <paolo.morettin@kuleuven.be>
2022-02-22 11:04:16 +0100

    teoria e pratica

--- uai-22/generate_dets.py
+++ uai-22/generate_dets.py
@@ -2,7 +2,7 @@
 from det import DET
 import numpy as np
 import os
-from pysmt.shortcuts import BOOL, LE, Plus, REAL, Real, Symbol, Times, reset_env
+from pysmt.shortcuts import *
 from pywmi import Density
 from scipy.linalg import solve as solve_linear_system
 from sys import argv
@@ -27,9 +27,10 @@ def sample_uniform_hyperplane(polytope):
 
 
 # full MLC suite sorted by increasing number of features
-EXPERIMENTS = ['balance-scale', 'iris', 'cars', 'diabetes', 'breast-cancer', 'glass2', 'glass',
-               'breast', 'solar', 'cleve', 'heart', 'australian', 'crx', 'hepatitis', 'german',
-               'german-org', 'auto', 'anneal-U']
+EXPERIMENTS = {'small' : ['balance-scale', 'iris', 'cars', 'diabetes', 'breast-cancer',
+                          'glass2', 'glass', 'breast', 'solar', 'cleve', 'hepatitis'],
+               'big' : ['heart', 'australian', 'crx', 'german', 'german-org', 'auto',
+                        'anneal-U']}
 
 DATAFOLDER = 'mlc-datasets'
 
@@ -41,66 +42,71 @@ if len(argv) != 6:
 
 nmin, nmax, nqueries, qhardness, seed = int(argv[1]), int(argv[2]), int(argv[3]), float(argv[4]), int(argv[5])
 
-benchmark_folder = f'dets-{nmin}-{nmax}-{nqueries}-{qhardness}-{seed}'
 
-if not os.path.isdir(benchmark_folder):
-    os.mkdir(benchmark_folder)
+for size in EXPERIMENTS:
+    benchmark_folder = f'{size}-dets-{nmin}-{nmax}-{nqueries}-{qhardness}-{seed}'
 
-for exp in EXPERIMENTS:
+    if not os.path.isdir(benchmark_folder):
+        os.mkdir(benchmark_folder)
 
-    # fresh pysmt environment
-    reset_env()
+    for exp in EXPERIMENTS[size]:
+        # fresh pysmt environment
+        reset_env()
     
-    detfile = os.path.join(benchmark_folder, f'{exp}-{nmin}-{nmax}.json')
+        detfile = os.path.join(benchmark_folder, f'{exp}-{nmin}-{nmax}.json')
 
-    if os.path.isfile(detfile):
-        print(f"{detfile} exists. Skipping.")
-        continue
+        if os.path.isfile(detfile):
+            print(f"{detfile} exists. Skipping.")
+            continue
 
-    print(f"{exp} : Parsing data")    
-    featfile = os.path.join(DATAFOLDER, f'{exp}.features')
-    feats = read_feats(featfile)
-    train = read_data(os.path.join(DATAFOLDER, f'{exp}.train.data'), feats)
-    valid = read_data(os.path.join(DATAFOLDER, f'{exp}.valid.data'), feats)
-    # we don't need this
-    # test = read_data(os.path.join(DATAFOLDER, f'{exp}.test.data'), feats) 
+        print(f"{exp} : Parsing data")    
+        featfile = os.path.join(DATAFOLDER, f'{exp}.features')
+        feats = read_feats(featfile)
+        train = read_data(os.path.join(DATAFOLDER, f'{exp}.train.data'), feats)
+        valid = read_data(os.path.join(DATAFOLDER, f'{exp}.valid.data'), feats)
 
-    print(f"{exp} : Learning DET({nmin},{nmax})")
-    det = DET(feats, nmin, nmax)
-    det.grow_full_tree(train)
-    det.prune_with_validation(valid)
+        print(f"{exp} : Learning DET({nmin},{nmax})")
+        det = DET(feats, nmin, nmax)
+        det.grow_full_tree(train)
+        det.prune_with_validation(valid)
 
-    domain, support, weight = det.to_pywmi()
-    queries = []
-    for i in range(nqueries):
-        np.random.seed(seed+i)
-        bbox = [domain.var_domains[v] for v in domain.real_vars]
-        nvars = len(bbox)
-        p = np.array([np.random.uniform(l, u) for l, u in bbox])
-        # uniformly sampled orientation for the hyperplane
-        o = np.random.uniform(0, 1, (nvars-1, nvars))
-        # coefficients for the system of equations (i.e. n points in ndimensions)
-        Points = p * np.concatenate((np.ones((1, nvars)), o))
+        domain, support, weight = det.to_pywmi()
+        queries = []
+        i = 0
+        #for i in range(nqueries):
+        np.random.seed(seed)
+        while i < nqueries:
+            bbox = [domain.var_domains[v] for v in domain.real_vars]
+            nvars = len(bbox)
+            p = np.array([np.random.uniform(l, u) for l, u in bbox])
+            # uniformly sampled orientation for the hyperplane
+            o = np.random.uniform(0, 1, (nvars-1, nvars))
+            # coefficients for the system of equations (i.e. n points in ndimensions)
+            Points = p * np.concatenate((np.ones((1, nvars)), o))
 
-        # solving the system to retrieve the hyperplane's coefficients
-        # [p1 ; ... , pn] * coeffs = 1
-        w = solve_linear_system(Points, np.ones((nvars, 1))).transpose()[0]
+            # solving the system to retrieve the hyperplane's coefficients
+            # [p1 ; ... , pn] * coeffs = 1
+            w = solve_linear_system(Points, np.ones((nvars, 1))).transpose()[0]
 
-        # consider a subset maybe?
-        selected = np.random.choice(nvars, int(nvars*qhardness), replace=False)
-        if len(selected) == 0:
-            selected = [np.random.choice(nvars)]
+            # consider a subset maybe?
+            selected = np.random.choice(nvars, int(nvars*qhardness), replace=False)
+            if len(selected) == 0:
+                selected = [np.random.choice(nvars)]
 
-        wx = [Times(Real(float(w[j])), x)
-              for j,x in enumerate(domain.get_real_symbols())
-              if j in selected]
-        query = LE(Plus(*wx), Real(1))
-        queries.append(query)
-    
-    density = Density(domain, support, weight, queries)
+            wx = [Times(Real(float(w[j])), x)
+                  for j,x in enumerate(domain.get_real_symbols())
+                  if j in selected]
+            query = LE(Plus(*wx), Real(1))
 
-    density.to_file(detfile)  # Save to file
-    # density = Density.from_file(filename)  # Load from file
+            if is_sat(And(support, query)):
+                queries.append(query)
+                i += 1
+            else:
+                print(f"UNSAT {i+1}/{nqueries}")
+            
+        density = Density(domain, support, weight, queries)
+        density.to_file(detfile)  # Save to file
+        # density = Density.from_file(filename)  # Load from file
 
     
 
