1d0c82a34047afbcd3768302bffb176955fcc355
paolomorettin <paolo.morettin@kuleuven.be>
2022-10-27 14:00:38 +0200

    probabilities seem off

--- wmibench/fairsquare/example.py
+++ wmibench/fairsquare/example.py
@@ -23,7 +23,16 @@ for prog in [unfair, fair]:
     p_MH, _ = wmi.computeWMI(MH, mode=WMI.MODE_SA_PA_SK)
     p_nMH, _ = wmi.computeWMI(nMH, mode=WMI.MODE_SA_PA_SK)
 
-    ratio = (p_MH / p_M) / (p_nMH / (1 - p_M))
+    p_H_g_M = p_MH / p_M
+    p_H_g_nM = p_nMH / (1 - p_M)
 
-    print("Ratio:", ratio)
+    print("Pr(M):", p_M)
+    print("Pr(H|M):", p_H_g_M)
+    print("Pr(H|~M):", p_H_g_nM)
+
+    ratio = p_H_g_M / p_H_g_nM
+
+    eps_fairness = (ratio > (1 - epsilon))
+
+    print("Ratio:", ratio, f"{epsilon}-fairness:", eps_fairness,'\n')
 
